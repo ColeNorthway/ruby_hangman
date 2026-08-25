bde9857b485f1d90e0d05b9d07ed5e01062e78d1
[Cole Northway] <[cenorthway289@gmail.com]>
2026-08-25 18:50:44 -0500

    Start Planning Save

--- hangman.rb
+++ hangman.rb
@@ -4,7 +4,7 @@ require_relative 'lib/ui'
 
 ALPHABET = %w[a b c d e f g h i j k l m n o p q r s t u v w z y x].freeze
 
-def get_letter(bad_letters)
+def get_input(bad_letters)
   loop do
     print 'Enter a letter: '
     letter = gets.chomp.strip.downcase
@@ -24,7 +24,7 @@ def main
 
   loop do
     ui.print_res(lives, bad_letters)
-    letter = get_letter(bad_letters)
+    letter = get_input(bad_letters)
     unless word.submit(letter)
       bad_letters.push(letter)
       lives -= 1
@@ -35,3 +35,10 @@ end
 
 # ENTRY
 main
+
+=begin
+1) Add Game Saver module
+  - Load and save class
+2) Add prompt to save game
+3) Save via json
+=end
---
+++ lib/saver.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Saver
+  def load
+
+  end
+
+  def save
+
+  end
+end
